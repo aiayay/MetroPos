6c93951aa159cefe3c1fa41394fc849e6fe4cd30
FIKRI-24 <irkif1011@gmail.com>
2024-08-27 00:02:16 +0700

    .

---
+++ backend/models/keranjang.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/dbconfig.js');
+
+const Keranjang = sequelize.define('Keranjang', {
+  id_keranjang: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  id_produk: DataTypes.STRING,
+  id_member: DataTypes.STRING,
+  kuantitas: DataTypes.INTEGER,
+  total_bayar: DataTypes.INTEGER,
+}, {
+  tableName: 'keranjang',
+  timestamps: false,
+});
+
+module.exports = Keranjang;
