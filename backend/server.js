2866b430e750357144b211d188aa04cd3ca873a8
FIKRI-24 <irkif1011@gmail.com>
2024-08-28 16:03:58 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,5 @@ const PORT = process.env.PORT || 3000
 //server
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}`);
+});
