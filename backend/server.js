b80e40da2f2406f3d5ab2304616f6ad376a27c96
FIKRI-24 <irkif1011@gmail.com>
2024-08-28 16:58:26 +0700

    updateee

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
+// Inisialisasi Express
 const app = express();
 
 // Middleware
@@ -8,21 +9,37 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routers
-const router = require("./routes/productrouter.js");
-app.use("/api/products", router);
 
-// Testing API
-app.get("/", (req, res) => {
-  res.json({ message: "hello " });
-});
 
-//port
+// Import semua router
+// const detailTransaksiRoutes = require('./routes/detailTransaksiRoutes');
+// const keranjangRoutes = require('./routes/keranjangRoutes');
+// const produkRoutes = require('./routes/produkRoutes');
+// const kategoriRoutes = require('./routes/kategoriRoutes');
+// const pembelianRoutes = require('./routes/pembelianRoutes');
+// const supplierRoutes = require('./routes/supplierRoutes');
+// const transaksiRoutes = require('./routes/transaksiRoutes');
+// const memberRoutes = require('./routes/memberRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
-const PORT = process.env.PORT || 3000;
 
-//server
+// Gunakan router yang diimport
+// app.use('/api/detailTransaksi', detailTransaksiRoutes);
+// app.use('/api/keranjang', keranjangRoutes);
+// app.use('/api/produk', produkRoutes);
+// app.use('/api/kategori', kategoriRoutes);
+// app.use('/api/pembelian', pembelianRoutes);
+// app.use('/api/supplier', supplierRoutes);
+// app.use('/api/transaksi', transaksiRoutes);
+// app.use('/api/member', memberRoutes);
+// app.use('/api/user', userRoutes);
 
+// Koneksi ke database
+const db = require('./models');
+db.sequelize.sync();
+
+// Start server
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
