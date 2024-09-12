c36f3e772d4465c591a3c6a1ba7c9ca1c1e06898
FIKRI-24 <irkif1011@gmail.com>
2024-09-12 14:12:21 +0700

    .

--- backend/routes/userrouter.js
+++ backend/routes/userrouter.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/usercontroller.js');
 const authMiddleware = require('../middlewares/authMiddleware.js');
-
 // Register user
 router.post('/register', userController.registerUser);
 
 // Login user
 router.post('/login', userController.loginUser);
 
+// Get all users (without auth middleware for now)
+router.get('/users', userController.getAllUsers);
+
 // Protected route (must be logged in)
-router.get('/profile', authMiddleware, userController.getUserProfile);
+router.get('/profile', userController.getUserProfile);
 
 module.exports = router;
