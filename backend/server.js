a197805fc01267ec2ff748693d2df03e227c1211
FIKRI-24 <irkif1011@gmail.com>
2024-09-13 14:48:08 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,22 @@ require('dotenv').config();
 // Inisialisasi Express
 const app = express();
 
-<<<<<<<<< Temporary merge branch 1
-var corsOptions = {
-    origin: 'http://localhost:8081'
-}
-
-// middleware
-app.use(cors(corsOptions))
-=========
 // Konfigurasi CORS
 const corsOptions = {
   origin: 'http://localhost:3000',
   methods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   credentials: true,
 };
->>>>>>>>> Temporary merge branch 2
 
 // Middleware
 app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Headers', '*');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
