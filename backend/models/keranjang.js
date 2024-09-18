1272a4ed3ff371a684365e06d56399c326e789ca
aiayay <141732687+aiayay@users.noreply.github.com>
2024-09-18 16:03:02 +0700

    .

--- backend/models/keranjang.js
+++ backend/models/keranjang.js
@@ -1,38 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
-  const Keranjang = sequelize.define('Keranjang', {
-    id_keranjang: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
+  const Keranjang = sequelize.define(
+    "Keranjang",
+    {
+      id_keranjang: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      id_member: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      id_produk: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      kuantitas: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
     },
-    id_member: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    id_produk: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    kuantitas: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1,
-    },
-  }, {
-    tableName: 'keranjang',
-    timestamps: true,
-  });
+    {
+      tableName: "keranjang",
+      timestamps: true,
+    }
+  );
 
   Keranjang.associate = (models) => {
     Keranjang.belongsTo(models.Member, {
-      foreignKey: 'id_member',
-      as: 'member',
+      foreignKey: "id_member",
+      as: "member",
     });
 
     Keranjang.belongsTo(models.Produk, {
-      foreignKey: 'id_produk',
-      as: 'produk',
+      foreignKey: "id_produk",
+      as: "produk",
     });
   };
 
