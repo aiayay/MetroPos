b112f2945dd8a66390991dd5d72db06ee2afc7b6
aiayay <141732687+aiayay@users.noreply.github.com>
2024-09-19 00:36:36 +0700

    .

--- frontend/src/components/FormEditPembelian.jsx
+++ frontend/src/components/FormEditPembelian.jsx
@@ -58,7 +58,7 @@ const FormEditPembelian = () => {
       try {
         const response = await axios.get(API_URL + "produk");
         console.log(response.data); // Cek apakah data produk muncul
-        setProdukList(response.data.data || []); // Default ke array kosong jika data tidak ada
+        setProdukList(response.data || []); // Default ke array kosong jika data tidak ada
       } catch (error) {
         console.error("Error fetching produk", error);
       }
