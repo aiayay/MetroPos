1474e9d5c4ac995a58a2ab16c65abfdb5aba720e
aiayay <141732687+aiayay@users.noreply.github.com>
2024-10-04 15:08:58 +0700

    .

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -14,6 +14,7 @@ const Welcome = () => {
   useEffect(() => {
     getTransaksi();
   }, []);
+
   const getTransaksi = async () => {
     const response = await axios.get(API_URL + "transaksi");
     setTransaksi(response.data);
